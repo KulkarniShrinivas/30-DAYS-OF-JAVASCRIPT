2e81d2175f7616528f76bf20ee2db259bf53c9b3
Shrinivas Kulkarni <91586785+KulkarniShrinivas@users.noreply.github.com>
2024-07-18 19:17:21 +0530

    Updated Multidimentional array

---
+++ Day-6/MultiDimensional-Arrays.js
@@ -0,0 +1,12 @@
+
+
+//create a two dimensional array(matrix) and log the entire array to the console
+
+var a1=[[1,2,3],[4,5,6],[7,8,9]];
+console.log(a1);
+
+
+//Access and log a specific element from the two dimensional array
+
+console.log("Below is the specific element from the two dimensional array: ");
+console.log(a1[1][2]);
