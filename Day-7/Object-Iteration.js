f137dd8ac20005e6bbd32ef2d72c06e74b311a7d
Shrinivas Kulkarni <91586785+KulkarniShrinivas@users.noreply.github.com>
2024-07-19 17:38:40 +0530

    Updated Iterations

--- Day-7/Object-Iteration.js
+++ Day-7/Object-Iteration.js
@@ -1,2 +1,22 @@
 
-//Add a method to the book object that uses the this key
+//Uses a for...in loop to iterate over the properties of the book object and log each property and its value
+
+console.log("Iterating over the properties of the book object:");
+
+var book = {
+    title: "The Alchemist",
+    author: "Paulo Cosco",
+    year: 1988
+
+};
+
+for(var key in book){
+    console.log(key +" : "+ book[key]);
+}
+
+
+//Use object.keys and object.values methods to log the keys and values of the book object 
+
+console.log("The keys of the book object are: "+ Object.keys(book));
+console.log("The values of the book object are: "+ Object.values(book));
+
